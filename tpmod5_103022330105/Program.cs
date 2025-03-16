ea6c617409981b39d86ef5be2dcd3b56427a01a8
Naufal Fahreza <naufalfahreza328@gmail.com>
2025-03-16 16:57:39 +0700

    merge berhasil

--- tpmod5_103022330105/Program.cs
+++ tpmod5_103022330105/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tesimpan adalah : {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Naufal");
+        DataGeneric<string> dataNim = new DataGeneric<string>("103022330105");
+        dataNim.PrintData();
+    }
+}
